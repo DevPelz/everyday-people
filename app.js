66872897f5b84e1848cffe300291810936fe6332
pelzfx <111582303+pelzfx@users.noreply.github.com>
2022-09-20 20:06:41 +0100

    my first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: './iq.jpg',
+    image2: '/pp.jpg',
+    image3: '/bb.jpg',
+    displacementImage: './zz.png',
+    
+})
